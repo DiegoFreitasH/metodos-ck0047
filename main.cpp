df80ed44fcc28a0cbb8bf4b36813942df5a6c957
Diego Holanda <diegoholanda2008@hotmail.com>
2019-10-15 17:58:23 -0300

    WIP - Implemented the possibility of reading and testing n functions.

--- main.cpp
+++ main.cpp
@@ -120,6 +120,47 @@ double newtonPolinomios(Equation eq, double x, double error){
 }
 
 int main(){
+    int n, i, mult;
+    double coef[4];
+    double raiz_nr, raiz_sec, raiz_np, x0, error;
+
+    cout << "Entre a quatidade de Equações: ";
+    cin >> n;
+    Equation equations[n];
+    for(i = 0 ; i < n ; i++){
+        cout << "Entre os coeficientes separados por espaços(a0 a1 a2 a3 a4): ";
+        cin >> coef[0] >> coef[1] >> coef[2] >> coef[3] >> coef[4];
+        cout << "Entre a precisão: ";
+        cin >> error;
+        cout << "Entre a multiplicidade: ";
+        cin >> mult;
+        equations[i] = Equation(coef[0], coef[1], coef[2], coef[3], coef[4], error, mult);
+    }
+
+    for(i = 0 ; i < n ; i++){
+        error = equations[i].getError();
+        mult = equations[i].getMult();
+        x0 = equations[i].isolation();
+        cout << "Isolamento: "<< x0;
+
+
+        cout << "Método Newton-Raphson: " << endl;
+        raiz_nr = newtonRaphson(equations[i], x0, mult, error);
+
+        cout << endl << "Método da Secante: " << endl;
+        raiz_sec = secante(equations[i], x0, x0 + 1, mult, error);
+
+        cout << endl << "Método Newton para Polinomios: " << endl;
+        raiz_np = newtonPolinomios(equations[i], x0, error);
+
+        cout << endl << "Raizes" << endl << "-----------" << endl;
+        cout << "Newton-Raphson: " << raiz_nr << endl;
+        cout << "Método da Secante: " << raiz_sec << endl;
+        cout << "Método Newton Polinomios: " << raiz_np << endl;
+        cout << "----------------------------------" << endl;
+    }
+    
+    /*
     double raiz_nr, raiz_sec, raiz_np;
     Equation eq_newraph = Equation(-2, 0, 1, 0, 0);
     Equation eq_sec = Equation(3, -9, 0, 1, 0);
@@ -127,6 +168,7 @@ int main(){
     double newraph_x0 = eq_newraph.isolation();
     double sec_x0 = eq_sec.isolation();
     double newpoli_x0 = eq_newpoli.isolation();
+
     cout << "Isolamento: " << endl;
     cout << "Newton-Raphson: " << newraph_x0 << endl;
     cout << "Secante: " << sec_x0 << endl;
@@ -147,7 +189,8 @@ int main(){
     cout << "Método da Secante: " << raiz_sec << endl;
     cout << "Método Newton Polinomios: " << raiz_np << endl;
     cout << "Localizando as Raizes: " << endl;
-    cout << eq_sec.localizeRoots() << endl;   
+    cout << eq_sec.localizeRoots() << endl; 
+    */  
 
     return 0;
 }
